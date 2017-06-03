840beb42a9be411c448cf0d169e26587ca20d20e
Yuta Nakano <yutanakano395@icloud.com>
2017-06-03 22:25:15 +0900

    カラースキーム用のDirectoryを作成

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # vimのディレクトリ構造を構築
-mkdir -p ~/vim/autoload/{init/.gitkeep,languages/.gitkeep,plugins/.gitkeep}
+mkdir -p ~/vim/autoload/{init/.gitkeep,colors/.gitkeep,languages/.gitkeep,plugins/.gitkeep}
 # .vimrcをコピー
 cp ~/vim-setup/.vimrc ~/vim/.vimrc
 # plugin設定ファイルを作成
@@ -10,7 +10,7 @@ mkdir ~/.cache/dein && cd ~/.cache/dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 # dein.vimの実行結果をplugin.vimに整形して書き込む
 trap 'rm "$TMP"' EXIT
-TMP=`mktemp` 
+TMP=`mktemp`
 sh ./installer.sh ~/.cache/dein > $TMP
 sed -n -e "/dein Scripts/,/End dein Scripts/p" $TMP > ~/vim/autoload/init/plugin.vim
 echo 'vim-setup setup.sh done'
